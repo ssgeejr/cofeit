4399caa21be7a3b705e9b3639760132e44e7f12a
Steven Gee <steve.gee@t-mobile.com>
2021-01-19 18:11:07 -0600

    set create filesystem script to be configurable

--- create_file_system.sh
+++ create_file_system.sh
@@ -1,33 +1,38 @@
 #!/bin/bash
 
+user=cicdops
+group=devops
+cofeitdir=${cofeitdir}
+
+
 echo "Creating Application home ..."
-mkdir -p /opt/apps/cofeit
+mkdir -p ${cofeitdir}
 
 echo "Creating docker registry home ..."
-mkdir /opt/apps/cofeit/registry
+mkdir ${cofeitdir}/registry
 
 echo "Creating Postgres data home ..."
-mkdir /opt/apps/cofeit/postgresql
-mkdir /opt/apps/cofeit/postgresql/data
+mkdir ${cofeitdir}/postgresql
+mkdir ${cofeitdir}/postgresql/data
 
 echo "Creating SonarQube home ..."
-mkdir /opt/apps/cofeit/sonarqube
-mkdir /opt/apps/cofeit/sonarqube/conf
-mkdir /opt/apps/cofeit/sonarqube/data
-mkdir /opt/apps/cofeit/sonarqube/extensions
-mkdir /opt/apps/cofeit/sonarqube/lib
-mkdir /opt/apps/cofeit/sonarqube/lib/bundled-plugins
+mkdir ${cofeitdir}/sonarqube
+mkdir ${cofeitdir}/sonarqube/conf
+mkdir ${cofeitdir}/sonarqube/data
+mkdir ${cofeitdir}/sonarqube/extensions
+mkdir ${cofeitdir}/sonarqube/lib
+mkdir ${cofeitdir}/sonarqube/lib/bundled-plugins
 
 echo "Creating Jenkins home ..."
-mkdir /opt/apps/cofeit/jenkins_home
+mkdir ${cofeitdir}/jenkins_home
 
 
 echo "Setting ownership and permission ..."
-chown -R cicdops:devops /opt/apps/cofeit
+chown -R ${user}:${group} ${cofeitdir}
 
 
 chmod 775 /opt
-chgrp devops /opt
-chgrp devops /opt/apps
-chown -R cicdops:devops /opt/apps/cofeit
-chown 1000:1000 /opt/apps/cofeit/jenkins_home
+chgrp ${group} /opt
+chgrp ${group} /opt/apps
+chown -R ${user}:${group} ${cofeitdir}
+chown 1000:1000 ${cofeitdir}/jenkins_home
